01ade62e07041c5a47255409f5ad0f154e6254b1
Gustavo Rizzo <140224461+guhrizzo@users.noreply.github.com>
2026-01-22 22:10:23 -0300

    remove

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="">
-      <h1 className="font-poppins text-5xl font-semibold">Texto com Poppins</h1>
+      <h1 className=""></h1>
 
     </div>
   );
