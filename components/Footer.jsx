6493984be2577dd31c570c4d2211defe91993bcc
Gustavo Rizzo <140224461+guhrizzo@users.noreply.github.com>
2026-01-24 18:54:52 -0300

    update navbar

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
-        <footer className="bg-[#003566ff] p-4 mt-8">
-            <div className="container mx-auto text-center text-white">
+        <footer className="bg-[#003566ff] p-4 mt-8 fixed bottom-0 w-full">
+            <div className="container mx-auto text-center text-white ">
                 &copy; {new Date().getFullYear()} Clube de Tiro. Todos os direitos reservados.
             </div>
         </footer>
