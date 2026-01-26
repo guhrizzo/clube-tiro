0ab8d3a5246b52d0698fbda1f948a7e3eedb398b
Gustavo Rizzo <140224461+guhrizzo@users.noreply.github.com>
2026-01-25 21:07:35 -0300

    Update page.js

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -2,8 +2,7 @@ import NavBar from "../../../components/NavBar";
 import Footer from "../../../components/Footer";
 import ContactCTA from "../../../components/ContactCTA";
 
-import ServiceCarousel from "../../../components/ServiceCarousel";
-import BackToTop from "../../../components/BackTop";
+
 import ScrollToTop from "../../../components/BackTop";
 
 
